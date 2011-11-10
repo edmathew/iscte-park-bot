bc26676a8aeed7f5a0cb4ba5455e2bdcd740794f
piresfm@gmail.com <piresfm@gmail.com@7d05f2e1-fcbf-242e-3c8b-b4b07b514ae7>
2011-11-10 11:59:26 +0000

    Created our own neural network algorithm. Still missing a lot of stuff. Including Activation Functions (and all the learning thing-a-magic).

---
+++ trunk/ park-bot --username piresfm@gmail.com/ParkBot/src/brain/BrainTester.java
@@ -0,0 +1,25 @@
+package brain;
+
+import matrix.Matrix;
+import feed_fwd.FeedforwardLayer;
+
+public class BrainTester {
+public static void main(String[] args) {
+	FeedForward ff = new FeedForward();
+	FeedForwardLayer input = new FeedForwardLayer(2);
+	FeedForwardLayer output = new FeedForwardLayer(2);
+	
+	double[][] outputLayerMatrixDef = {
+			{2, 0},
+			{1, 2}
+	};
+	Matrix outputLayerMatrix = new Matrix(outputLayerMatrixDef); 
+	output.setMatrix(outputLayerMatrix);
+	ff.addLayer(input);
+	ff.addLayer(output);
+	
+	double[] inputData = {2,2};
+	double[] result = ff.calculate(inputData);
+	System.out.println(result[0] + " " + result[1]);
+}
+}
